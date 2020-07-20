320dbfe1263dbfecd9cd8b55427fed923f694a37
Lorna Armstrong <s1812323@ed.ac.uk>
2020-07-20 14:06:51 +0000

    Add project section and languages.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
+
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
